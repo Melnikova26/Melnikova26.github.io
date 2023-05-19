c5560dec3adf9810098266dc241b555f56fd4fa1
Melnikova26 <mmelnekowa@gmail.com>
2023-05-19 15:33:23 +0500

    fix mistakes

--- components/map.js
+++ components/map.js
@@ -1,46 +1,27 @@
 function renderMapPage(element) {
-    element.innerHTML = `
-    <div class="wrapper">
-        <div class="map w-100">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <div>Basic map</div>
-                <div class="d-flex align-items-center">
-                    <div class="me-2 btn p-0">
-                        <img src="./assets/icons/arrow-down.svg" alt="Arrow">
-                    </div>
-                    <div class="me-2 btn p-0">
-                        <img src="./assets/icons/update.svg" alt="Update">
-                    </div>
-                    <div class="btn p-0">
-                        <img src="./assets/icons/close.svg" alt="Close">
-                    </div>
-                </div>
-            </div>
-            <div id="map" class="w-100 d-flex justify-content-center align-items-center map__main" style="height: 480px">
-                <div class="spinner-border"></div>
-            </div>
-        </div>
-    </div>
-    `;
-    const loading = document.querySelector('.spinner-border');
-    console.log(loading);
-    const center = [55.74610820643666,37.63562416176467];
+    fetch('./pages/map.html')
+        .then(response => response.text())
+        .then(res => {
+            element.innerHTML = res;
+            const loading = document.querySelector('.spinner-border');
+            const center = [55.74610820643666,37.63562416176467];
 
-    function init() {
-        const map = new ymaps.Map('map', {
-            center: center,
-            zoom: 16
-        });
-        const placemark = new ymaps.Placemark(center  ,{},{
-            iconLayout: 'default#image',
-            iconImageHref: './assets/icons/location.svg',
-            iconImageSize: [40, 40],
-            iconImageOffset: [-15, -39]
-        });
-        map.geoObjects.add(placemark);
-        map.controls.remove('trafficControl');
-        loading.style.visibility = 'hidden';
-    }
+            function init() {
+                const map = new ymaps.Map('map', {
+                    center: center,
+                    zoom: 16
+                });
+                const placemark = new ymaps.Placemark(center  ,{},{
+                    iconLayout: 'default#image',
+                    iconImageHref: './assets/icons/location.svg',
+                    iconImageSize: [40, 40],
+                    iconImageOffset: [-15, -39]
+                });
+                map.geoObjects.add(placemark);
+                map.controls.remove('trafficControl');
+                loading.style.visibility = 'hidden';
+            }
 
-    ymaps.ready(init);
+            ymaps.ready(init);
+        });  
 }
--- components/time.js
+++ components/time.js
@@ -1,58 +1,40 @@
 function renderTimePage(element) {
-    element.innerHTML = `
-    <div class="wrapper">
-        <div class="time w-100">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-                <div>Timer</div>
-                <div class="d-flex align-items-center">
-                    <div class="me-2 btn p-0">
-                        <img src="./assets/icons/arrow-down.svg" alt="Arrow">
-                    </div>
-                    <div class="me-2 btn p-0">
-                        <img src="./assets/icons/update.svg" alt="Update">
-                    </div>
-                    <div class="btn p-0">
-                        <img src="./assets/icons/close.svg" alt="Close">
-                    </div>
-                </div>
-            </div>
-            <div class="time__main">00:00:00</div>
-        </div>
-    </div>
-    `;
-    
-    function updateTimer() {
-        let time = localStorage.getItem('siteTimer');
-        
-        if (time) {
-            time = parseInt(time, 10) + 1;
-        } else {
-            time = 0;
-        }
+    fetch('./pages/time.html')
+        .then(response => response.text())
+        .then(res => {
+            element.innerHTML = res;
+            
+            function updateTimer() {
+                let time = localStorage.getItem('siteTimer');
+                
+                if (time) {
+                    time = parseInt(time, 10) + 1;
+                } else {
+                    time = 0;
+                }
 
-        localStorage.setItem('siteTimer', time.toString());
+                localStorage.setItem('siteTimer', time.toString());
 
-        const timerElement = document.querySelector('.time__main');
-        timerElement.textContent = formatTime(time);
-    }
-    
-      
-    function formatTime(time) {
-        const hours = Math.floor(time / 3600);
-        const minutes = Math.floor((time % 3600) / 60);
-        const seconds = time % 60;
-        
-        return `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
-    }
-      
-    function addZero (num){
-        return +num < 10 ? `0${num}` : num;
-    } 
-    const resetTimer = () => localStorage.removeItem('siteTimer');
-    const timer = setInterval(updateTimer, 1000);
-    window.addEventListener('beforeunload', () => {
-        resetTimer();
-        clearInterval(timer);
-    });
-
+                const timerElement = document.querySelector('.time__main');
+                timerElement.textContent = formatTime(time);
+            }
+            function formatTime(time) {
+                const hours = Math.floor(time / 3600);
+                const minutes = Math.floor((time % 3600) / 60);
+                const seconds = time % 60;
+                
+                return `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
+            }
+            
+            function addZero (num){
+                return +num < 10 ? `0${num}` : num;
+            } 
+            const resetTimer = () => localStorage.removeItem('siteTimer');
+            
+            window.addEventListener('beforeunload', () => {
+                resetTimer();
+                clearInterval(timer);
+            });
+            const timer = setInterval(updateTimer, 1000);
+        });
 }
