569ada014d6f0504cb7e2d8624b412ab553a33ee
Melnikova26 <mmelnekowa@gmail.com>
2023-05-20 21:08:46 +0500

    fix time page

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,3 +1,4 @@
+
 const links = document.querySelectorAll('.header__nav-item'),
       linkElems = document.querySelectorAll('.header__nav-link');
 
@@ -5,6 +6,8 @@ linkElems.forEach((item, i) => {
     if (item.href.slice(item.href.indexOf('#')) === location.hash){
         getLink(i);
         links[i].click();
+    } else if (location.hash == ''){
+        getLink();
     }
 })
 
@@ -14,6 +17,48 @@ function getLink(i = 0){
 }
 links.forEach((item, i) => {
     item.addEventListener('click', () => getLink(i));
+});
+let startTime;
+window.addEventListener('load', () => {
+    if (!localStorage.getItem("startTime")) {
+        startTime = new Date();
+        localStorage.setItem("startTime", startTime);
+    } else {
+        startTime = new Date(localStorage.getItem("startTime"));
+    }
+});
+
+function getTime(startTime) {
+    const total = Date.parse(new Date()) - Date.parse(startTime);
+    hours = Math.floor((+total / (1000 * 60 * 60)) % 24),
+    minutes = Math.floor((+total / (1000 * 60)) % 60),
+    seconds = Math.floor((+total / 1000 % 60));
+      
+    return {
+        'total': total,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
+}
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+function setClock(selector) {
+    const timer = document.querySelector(selector);
+    const t = getTime(startTime);
+    if (!t){
+        timer.innerHTML = `00:00:00`;
+    } else {
+        timer.innerHTML = `${getZero(t.hours)}:${getZero(t.minutes)}:${getZero(t.seconds)}`;
+    }
+}
+window.addEventListener("beforeunload", function () {
+    localStorage.removeItem("startTime");
 });
 
 function handleHashChange() {
@@ -28,7 +73,7 @@ function handleHashChange() {
             renderMapPage(contentElement);
             break;
         case '#time':
-            renderTimePage(contentElement);
+            renderTimePage(contentElement, setClock);
             break;
         default:
             renderHomePage(contentElement);
--- components/time.js
+++ components/time.js
@@ -1,40 +1,9 @@
-function renderTimePage(element) {
+function renderTimePage(element, setClock) {
     fetch('./pages/time.html')
         .then(response => response.text())
         .then(res => {
             element.innerHTML = res;
-            
-            function updateTimer() {
-                let time = localStorage.getItem('siteTimer');
-                
-                if (time) {
-                    time = parseInt(time, 10) + 1;
-                } else {
-                    time = 0;
-                }
-
-                localStorage.setItem('siteTimer', time.toString());
-
-                const timerElement = document.querySelector('.time__main');
-                timerElement.textContent = formatTime(time);
-            }
-            function formatTime(time) {
-                const hours = Math.floor(time / 3600);
-                const minutes = Math.floor((time % 3600) / 60);
-                const seconds = time % 60;
-                
-                return `${addZero(hours)}:${addZero(minutes)}:${addZero(seconds)}`;
-            }
-            
-            function addZero (num){
-                return +num < 10 ? `0${num}` : num;
-            } 
-            const resetTimer = () => localStorage.removeItem('siteTimer');
-            
-            window.addEventListener('beforeunload', () => {
-                resetTimer();
-                clearInterval(timer);
-            });
-            const timer = setInterval(updateTimer, 1000);
+            setClock('.time__main');
+            setInterval(() => setClock('.time__main'), 1000);
         });
 }
