66b67c7c8db8bdf64bddd0dd707875ef77d0b522
Melnikova26 <mmelnekowa@gmail.com>
2023-05-26 14:10:04 +0500

    add new js file timer and fixing mistakes

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,3 +1,4 @@
+import { setClock } from "./timer.js";
 async function fetchPage(url, element) {
     return await fetch(url)
                     .then(response => {
@@ -30,7 +31,7 @@ linkElems.forEach((item, i) => {
 
 function getLink(i = 0){
     linkElems.forEach(item => item.classList.remove('header__nav-link_active'));
-    linkElems[i].classList.add('header__nav-link_active');
+    linkElems[i]?.classList.add('header__nav-link_active');
 }
 
 function updatePage(path) {
@@ -52,59 +53,9 @@ function updatePage(path) {
     }
 }
 
-
 window.addEventListener('popstate', () => {
-    const path = location.pathname;
-    updatePage(path);
+    updatePage(location.pathname);
 });
-
-
-// Time
-let startTime;
-
-function getZero(num) {
-    if (num >= 0 && num < 10) {
-        return `0${num}`;
-    } else {
-        return num;
-    }
-}
-function getTime(startTime) {
-    const total = Date.parse(new Date()) - Date.parse(startTime);
-    hours = Math.floor((+total / (1000 * 60 * 60)) % 24),
-    minutes = Math.floor((+total / (1000 * 60)) % 60),
-    seconds = Math.floor((+total / 1000 % 60));
-      
-    return {
-        'total': total,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    }
-}
-
-function setClock(selector) {
-    const timer = document.querySelector(selector);
-    const t = getTime(startTime);
-    if (timer){
-        if (!t){
-            timer.innerHTML = `00:00:00`;
-        } else {
-            timer.innerHTML = `${getZero(t.hours)}:${getZero(t.minutes)}:${getZero(t.seconds)}`;
-        }
-    } 
-}
-
 window.addEventListener('load', () => {
     updatePage(location.pathname);
-    if (!localStorage.getItem("startTime")) {
-        startTime = new Date();
-        localStorage.setItem("startTime", startTime);
-    } else {
-        startTime = new Date(localStorage.getItem("startTime"));
-    }
-});
-
-window.addEventListener("beforeunload", function () {
-    localStorage.removeItem("startTime");
 });
